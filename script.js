d310b842eaee280dd6ff028bd1718afbe73d7710
fikratkhatira <fekratkhatira@gmail.com>
2024-07-05 20:11:54 -0700

    html codes cleaned, css and js first errors fixed

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-let userInput = document.getElementById('user-input');
-let checkBtn = document.getElementById('check-btn');
-let clearBtn = document.getElementById('clear-btn');
-let resultsDiv = document.getElementById('results-div');
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
 
 function checkNumber() {
   const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
 
-if (userInput.value == "") {
-  return alert('Please provide a phone number')
-}
-let item = document.createElement('li');
-resultsDiv.appendChild(item);
+  if (userInput.value === "") {
+    return alert('Please provide a phone number');
+  }
 
-if (regex.test(userInput.value)) {
-item.innerHTML = 'Valid US number: ' + userInput.value;
-item.classList.add('valid');
-userInput.value = "";
-} else {
- item.innerHTML = 'Invalid US number: ' + userInput.value;
- item.classList.add('invalid'); 
- userInput.value = "";
-}
+  let item = document.createElement('li');
+  resultsDiv.appendChild(item);
+
+  if (regex.test(userInput.value)) {
+    item.innerHTML = 'Valid US number: ' + userInput.value;
+    item.classList.add('valid');
+    userInput.value = "";
+  } else {
+    item.innerHTML = 'Invalid US number: ' + userInput.value;
+    item.classList.add('invalid');
+    userInput.value = "";
+  }
 }
 
 checkBtn.addEventListener('click', checkNumber);
 
 clearBtn.addEventListener('click', () => {
-resultsDiv.innerHTML = "";
-})
+  resultsDiv.innerHTML = "";
+});
